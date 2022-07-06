495a4b70e7891341b9995639e4453b4965483fd3
Kevin Nielsen <knielsen24@gmail.com>
2022-07-06 13:44:47 -0700

    commit tree 622a390b8084f2cc4bcc16ef1f14594642150329

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,9 @@
 class SessionsController < ApplicationController
-  
-  def index
-    session[:session_hello] ||= "World"
-    cookies[:cookies_hello] ||= "World"
-    render json: { session: session, cookies: cookies.to_hash }
-  end
+
+	def index
+		session[:session_hello] ||= "hello"
+		cookies[:cookies_hello] ||= "hello"
+		render json: { session: session, cookies: cookies.to_hash }
+	end
 
 end
